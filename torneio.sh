c3977798324e3d66b45b4fde3b3f3f9f8d6e16d7
Rui Mendes <azuki@di.uminho.pt>
2020-04-27 18:46:47 +0100

    O torneio já procura os logotipos e coloca-os nos filmes

--- torneio.sh
+++ torneio.sh
@@ -1,6 +1,16 @@
 rm -f jogo_*.log
 rm -f jogo_*.mp4
 
+get_logo() {
+	if [ -f "logos/$1.png" ]
+	then
+		logo=logos/$1.png
+	else
+		logo=~/Downloads/5.jpg
+	fi
+	echo $logo
+}
+
 if [ ! -d concorrentes ]
 then
 	echo "Erro: diretoria de concorrentes não existe!"
@@ -20,7 +30,9 @@ do
 			tail -n 1 jogo_${C1}_${C2}.log
 
 			cp inicial jogos/pos00
-			./render.py $C1 $C2 ~/Downloads/6.png ~/Downloads/5.jpg
+			logo1=$(get_logo $C1)
+			logo2=$(get_logo $C2)
+			./render.py $C1 $C2 $logo1 $logo2
 			ffmpeg -r 1 -i jogos/pos%02d.svg -pix_fmt yuv420p jogo.mp4 &> /dev/null
 			mv jogo.mp4 jogo_${C1}_${C2}.mp4
 		fi
